7c9156c355379e52ff80c2a1540508086e64c5ac
erobakiewicz <eliasz.robakiewicz@laboratorium.ee>
2022-01-18 17:31:13 +0100

    created model geolocation

--- ipstackAPI/settings.py
+++ ipstackAPI/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'locations.apps.LocationsConfig'
 ]
 
 MIDDLEWARE = [
@@ -114,3 +115,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+IPSTACK_ACCESS_KEY = env('IPSTACK_ACCESS_KEY')
--- locations/models.py
+++ locations/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Geolocation(models.Model):
+    ip = models.CharField(max_length=39, verbose_name="ip address")
+    continent_code = models.CharField(max_length=5, verbose_name="continent code")
+    continent_name = models.CharField(max_length=50, verbose_name="continent name")
+    country_name = models.CharField(max_length=100, verbose_name='country name')
+    city = models.CharField(max_length=250, verbose_name='city')
+
+    def __str__(self):
+        return f'{self.ip}: {self.country_name}'
