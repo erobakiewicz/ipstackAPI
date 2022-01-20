d2ef0ea1032a94b41c3ca08ff3ec832d3fd666be
erobakiewicz <eliasz.robakiewicz@laboratorium.ee>
2022-01-20 10:40:54 +0100

    setttings

--- ipstackAPI/settings.py
+++ ipstackAPI/settings.py
@@ -13,7 +13,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 DEBUG = env('DEBUG', False)
 
-ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=[])
+ALLOWED_HOSTS = ["*"]
 
 SECRET_KEY = env('SECRET_KEY')
 
